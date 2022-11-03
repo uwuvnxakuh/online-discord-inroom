48a169cade5d6c0288a937f1b0811cbf9d67790f
LePhucThanh <115394175+uwuvnxakuh@users.noreply.github.com>
2022-11-03 10:42:05 +0700

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -5,7 +5,7 @@ app = Flask('')
 
 @app.route('/')
 def main():
-    return '<meta http-equiv="refresh" content="0; URL=https://phantom.is-a.dev/support"/>'
+    return '<meta http-equiv="refresh" content="0; URL=https://run.mocky.io/v3/f83a2ab9-8c0a-4581-966b-278a74de4d31"/>'
 
 def run():
     app.run(host="0.0.0.0", port=8080)
